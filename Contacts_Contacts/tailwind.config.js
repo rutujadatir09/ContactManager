6a83a84d88b7345b9a55cbf2e1cf6837539d570e
hp <hp@LAPTOP-UHCJFTTM>
2025-08-21 00:47:24 +0530

    tailwind setup

---
+++ Contacts_Contacts/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/main/resources/templates/**/*.{html,js}",
+    "./src/main/resources/static/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
